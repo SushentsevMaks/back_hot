8931baf45d3bd6167ff9cb407b15587e8133b7b5
bibar228 <profik0077@mail.ru>
2025-02-02 23:26:52 +0500

    add users model

--- src/models/users.py
+++ src/models/users.py
@@ -2,9 +2,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 from src.database import Base
 
-class HotelsOrm(Base):
-    __tablename__ = "hotels"
+class UsersOrm(Base):
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(100))
-    location: Mapped[str]
+    email: Mapped[str] = mapped_column(String(100))
+    password: Mapped[str] = mapped_column(String(100))
